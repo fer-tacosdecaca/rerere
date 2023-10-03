a9e291bc8eea620a43f909ad72f356e0124e6fe6
fer-tacosdecaca <130688038+fer-tacosdecaca@users.noreply.github.com>
2023-10-02 20:03:21 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 function preload() {
-nise = loadimage("nuse.png")
+nise = loadImage("nuse.png")
 }
 
 function setup() {
 canvas = createCanvas(200, 200)
-  canvas.position(110, 150)
+  canvas.center()
   vdeo = createCapture(VIDEO)
   vdeo.hide()
   vdeo.size(200,200)
 
-
-
+  poseNet = ml5.poseNet(vdeo, modelLoaded)
+poseNet.on('pose' ,gotPoses)
 }
-
+function modelLoaded(){
+  console.log("https://makeameme.org/meme/youve-been-trolled-k5g4hx")
+}
+function gotPoses(results){
+  console.log(results)
+  if(results.length > 0) 
+  { console.log(results); noseX = results[0].pose.nise.x-15; noseY = results[0].pose.nise.y-15;
+}
 function draw() {
 image(vdeo,0,0,200,200)
-image(nise,0,0,10,2)
+image(nise,nise.X,nise.Y,10,2)
 
 }
   
